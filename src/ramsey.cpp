bf0e2c1b141b355d89cda963db9bdea3f423a25d
dougUCN <douglas.kt.wong@gmail.com>
2023-03-20 17:39:36 +0900

    Fix ramsey.cpp output to text

--- src/ramsey.cpp
+++ src/ramsey.cpp
@@ -1,8 +1,8 @@
 // Sample program that creates a ramsey fringe, either circular or linear
 //
 // Output: either linRamsey.txt or circRamsey.txt, depending on INT_ID choice
-// The text file will contain columns freq, zProb, and params
-// Params contains {0, W0_VAL, WL_VAL, PHI_VAL, INT_ID}
+// The text file will contain columns freq, zProb, and params 
+// {W0_VAL, WL_VAL, PHI_VAL, INT_ID}
 
 #include <iostream>
 #include <cmath>
@@ -29,6 +29,9 @@ const double PULSE_2_TIME = 4.286; // [seconds]
 const double RK_STEP = 0.001;      // [seconds]
 const double PRECESS_TIME = 180;   // [seconds]
 
+// Output precision to stdout and file
+const int PRECISION = 12;
+
 const double INT_ID = USE_LINEAR_RF; // Type of RF pulse (USE_CIRCULAR_RF or USE_LINEAR_RF)
 
 int main()
@@ -36,6 +39,7 @@ int main()
     vector<double> wOut, zOut, params;
     string filename;
     neutron ucn;
+    ofstream outfile;
     double phiVal2, wVal;
     double progress = 0.1;
 
@@ -82,9 +86,20 @@ int main()
     {
         filename = "circRamsey.txt";
     }
-    params = {0, W0_VAL, WL_VAL, PHI_VAL, INT_ID};
+
     cout << "...100%" << endl
          << "Saving output to " << filename << "...";
+    
+    outfile.open(filename);
+    outfile << "#W0_VAL=" << W0_VAL << ",WL_VAL=" << WL_VAL 
+            << ",PHI_VAL=" << PHI_VAL << ",INT_ID=" << INT_ID << "\n";
+    outfile.precision(PRECISION);
+    outfile << "w,zProb\n";
+
+    for (int i = 0; i < wOut.size(); i++)
+    {
+        outfile << wOut[i] << "," << zOut[i] << "\n";
+    }
 
     cout << "Done!\n";
 
