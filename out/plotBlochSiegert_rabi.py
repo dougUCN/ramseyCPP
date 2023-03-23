76f8c8a5685629eb60f21668d9b0f5c025721929
dougUCN <douglas.kt.wong@gmail.com>
2023-03-23 17:35:30 +0900

    Add bloch siegert rabi calculation

--- out/plotBlochSiegert_rabi.py
+++ out/plotBlochSiegert_rabi.py
@@ -10,13 +10,13 @@ def main():
     parser = argparse.ArgumentParser(description="Plots output from blochSiegert.cpp")
     parser.add_argument("-f", "--file", type=str, help="Filename", required=True)
     parser.add_argument(
-        "-rf", "--ramseyFringe", type=str, nargs="+", help="rf__.txt file(s) to draw"
+        "-rabi", "--rabiFringe", type=str, nargs="+", help="rabi__.txt file(s) to draw"
     )
     args = parser.parse_args()
 
     print(f"Loading {args.file}")
     df = pd.read_csv(
-        args.file, comment="#", names=["phi", "gridSearchMin", "polyFitMin"]
+        args.file, comment="#", names=["pulseWidth", "gridSearchMin", "polyFitMin"]
     )
     print(df)
     bloch_siegert_params = parse_params(args.file)
@@ -24,47 +24,46 @@ def main():
 
     plt.figure()
     plt.plot(
-        df["phi"].to_numpy(),
+        df["pulseWidth"].to_numpy(),
         bloch_siegert_params["W0_VAL"] - df["polyFitMin"].to_numpy(),
         label="Polynomial fit",
         color="#005F73",
     )
-    # plt.plot(
-    #     df["phi"].to_numpy(),
-    #     bloch_siegert_params["W0_VAL"] - df["gridSearchMin"].to_numpy(),
-    #     label="Grid search",
-    # )
+    plt.plot(
+        df["pulseWidth"].to_numpy(),
+        bloch_siegert_params["W0_VAL"] - df["gridSearchMin"].to_numpy(),
+        label="Grid search",
+    )
     plt.grid(True)
-    plt.xlabel(r"$\phi$ [rad]")
+    plt.xlabel(r"$\pi$ pulse width [s]")
     plt.ylabel(r"$\Delta$ Bloch-Sigert [rad/s]")
     plt.ticklabel_format(axis="y", useMathText=True)
-    ax = plt.gca()
-    ax.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
-    ax.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 4))
-    ax.xaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
-    # plt.legend()
+    plt.legend()
 
-    if args.ramseyFringe:
+    if args.rabiFringe:
         fringe = {}
-        for fringe_file in args.ramseyFringe:
+        for fringe_file in args.rabiFringe:
             print(f"Loading {fringe_file}")
-            phi = parse_params(fringe_file)["phi"]
-            fringe[phi] = pd.read_csv(
+            pulseWdith = parse_params(fringe_file)["pulseWidth"]
+            fringe[pulseWdith] = pd.read_csv(
                 fringe_file, comment="#", header=0, names=["w", "zProb"]
             )
 
             plt.figure()
-            plt.title(rf"$\phi$={phi} [rad]")
+            plt.title(rf"$t$={pulseWdith} [s]")
             plt.xlabel(r"$\omega$ [rad/s]")
             plt.ylabel("P(z)")
-            plt.plot(fringe[phi]["w"].to_numpy(), fringe[phi]["zProb"].to_numpy())
+            plt.plot(
+                fringe[pulseWdith]["w"].to_numpy(),
+                fringe[pulseWdith]["zProb"].to_numpy(),
+            )
             plt.axvline(
-                df.query("phi == @phi")["polyFitMin"].tolist()[0],
+                df.query("pulseWidth == @pulseWdith")["polyFitMin"].tolist()[0],
                 label="polyFitMin",
                 color="C1",
             )
             plt.axvline(
-                df.query("phi == @phi")["gridSearchMin"].tolist()[0],
+                df.query("pulseWdith == @pulseWdith")["gridSearchMin"].tolist()[0],
                 label="gridSearchMin",
                 color="C2",
             )
--- src/blochSiegert_rabi.cpp
+++ src/blochSiegert_rabi.cpp
@@ -1,16 +1,15 @@
-// Draws a graph of Bloch Siegert shift in optimized linear or circular ramsey fringes
-// as a function of the initial RF phase angle phi
+// Draws a graph of Bloch Siegert shift in optimized linear or circular rabi fringes
+// as a function of pi pulse time
 //
 // To determine the BS shift for a linear fringe, searches for the lowest
 // local minimun in the vicinity of w_0, the expected resonant frequency.
 //
-// Fits a quadratic polynomial to the bottom of the fringe. The central peak of
-// ramsey fringe has width 1/T, where T = precession period
+// Fits a quadratic polynomial to the bottom of the fringe.
 //
-// Outputs: blochSiegert.txt, with columns phi (rad), wRange (ramsey fringe freqs)
-//          gridMin(minimums on ramsey fringe from grid search),
-//          polyMin (minimums on ramsey fringe from fitting curve to polynomial),
-//          Header has params {W0_VAL, PRECESS_TIME, PULSE_TIME, INT_ID}
+// Outputs: blochSiegert_rabi.txt, with columns pulseWidth (s), wRange (ramsey fringe freqs)
+//          gridMin(minimums on rabi fringe from grid search),
+//          polyMin (minimums on rabi fringe from fitting curve to polynomial),
+//          Header has params {W0_VAL, INT_ID}
 //
 //          Separately outputs every ramsey fringe made, numbered
 //          rf1, rf2.... etc
@@ -25,16 +24,15 @@
 
 using namespace std;
 
-const double PHASE_INIT = 0;       // Initial phase angle to scan
-const double PHASE_FINAL = 2 * PI; // Final Phase angle to scane
-const double PHASE_STEP = 0.05;     // [rad]
+const double TIME_INIT = 1;  // Initial phase angle to scan
+const double TIME_FINAL = 6; // Final Phase angle to scane
+const double TIME_STEP = 0.05;   // [rad]
 
 // Ramsey Fringe parameters
-const double PRECESS_TIME = 180;  // Seconds
 const double W_STEP = 5e-7;       //[rad s^-1]    Step width of search around w0
 const int W_STEP_NUM = 100;       // Number of steps to search around w0 (both < and >= of W0_VAL)
 const double W0_VAL = 183.247172; //[rad s^-1]    Static field strength
-const double PULSE_TIME = 4.286;  //[seconds]  Time in which pi/2 pulse applied
+const double PHI_INIT = 0;        //[rad] Initial phase angle of RF
 
 // Integration parameters
 const double INT_ID = USE_LINEAR_RF; // Type of RF pulse (USE_CIRCULAR_RF or USE_LINEAR_RF)
@@ -46,7 +44,7 @@ const int PRECISION = 12;
 
 int main()
 {
-    vector<double> fringe, phaseRange, wRange, polyCoeff;
+    vector<double> fringe, tRange, wRange, polyCoeff;
     vector<double> wRangeAdj; // Used for polynomial fitting
     vector<double> gridSearchMin, polyFitMin;
     vector<double>::iterator min;
@@ -56,13 +54,12 @@ int main()
     double phiVal2, wl;
 
     // Vvectors of parameters to scan, calculate optimal ramsey pulse time
-    wl = PI / PULSE_TIME;
-    double temp = PHASE_INIT;
-    phaseRange.push_back(temp);
-    for (int i = 0; temp <= PHASE_FINAL; i++)
+    double temp = TIME_INIT;
+    tRange.push_back(temp);
+    for (int i = 0; temp <= TIME_FINAL; i++)
     {
-        temp += PHASE_STEP;
-        phaseRange.push_back(temp);
+        temp += TIME_STEP;
+        tRange.push_back(temp);
     }
     for (int i = 0; i < W_STEP_NUM * 2; i++)
     {
@@ -71,30 +68,30 @@ int main()
     }
 
     // For progress tracking
-    int phaseProgress = phaseRange.size();
+    int phaseProgress = tRange.size();
     int wProgress = wRange.size();
 
     // For file output
     if (INT_ID == USE_LINEAR_RF)
     {
-        filename = "linBlochSiegert.txt";
+        filename = "linBlochSiegertRabi.txt";
     }
     else
     {
-        filename = "circBlochSiegert.txt";
-        wl = (2 * PI) / PULSE_TIME;
+        filename = "circBlochSiegertRabi.txt";
     }
 
-    // Make a ramsey fringe for each value in phaseRange
-    int counterPhi = 1;
+    // Make a ramsey fringe for each value in tRange
+    int counterTime = 1;
     int counterW = 0;
     double progress;
-    for (auto phi : phaseRange)
+    for (auto t : tRange)
     {
+        wl = ((2 - INT_ID) * PI) / t;
         fringe.clear();
 
         // update progress
-        cout << "Fringe " << counterPhi << " / " << phaseRange.size() << endl;
+        cout << "Fringe " << counterTime << " / " << tRange.size() << endl;
         cout << "0%..." << flush;
         progress = 0.1;
         counterW = 0;
@@ -102,10 +99,8 @@ int main()
         for (auto wVal : wRange)
         {
             ucn.setState({1, 0, 0, 0});
-            phiVal2 = wVal * PULSE_TIME + phi + wVal * PRECESS_TIME;
-            ucn.integrate(PULSE_TIME, RK_STEP, {wVal, W0_VAL, wl, phi, INT_ID});
-            ucn.larmorPrecess(PRECESS_TIME, W0_VAL);
-            ucn.integrate(PULSE_TIME, RK_STEP, {wVal, W0_VAL, wl, phiVal2, INT_ID});
+            phiVal2 = wVal * t + PHI_INIT + wVal;
+            ucn.integrate(t, RK_STEP, {wVal, W0_VAL, wl, PHI_INIT, INT_ID});
             fringe.push_back(getZProb(ucn.getState()));
 
             // Output progress
@@ -127,10 +122,10 @@ int main()
         polyFitMin.push_back(-polyCoeff[1] / (2 * polyCoeff[2]) + W0_VAL);
 
         // Save output to file
-        branchname = "rf" + to_string(counterPhi) + ".txt";
+        branchname = "rf" + to_string(counterTime) + ".txt";
         outfile.open(branchname);
         outfile.precision(PRECISION);
-        outfile << "#phi=" << phi << "\n"
+        outfile << "#pulseWidth=" << t << "\n"
                 << "#w,zProb\n";
         for (int i = 0; i < wRange.size(); i++)
         {
@@ -139,26 +134,19 @@ int main()
         outfile.close();
 
         cout << "100%" << endl;
-
-        // // debugging
-        // for (auto coeff : polyCoeff) cout << coeff << "  ";
-        // cout << "\ngridSearchMin: ( " << setprecision(12) << polyFitMin.back()+W0_VAL << " , " << polyval(polyCoeff,{-polyCoeff[1]/(2*polyCoeff[2])})[0] << " )" << endl;
-        // cout << "gridSearchMin: ( " << setprecision(12) << gridSearchMin.back() << " , " << *min << " )" << endl;
-
-        counterPhi++;
+        counterTime++;
     }
 
     cout << "\nSaving output to " << filename << "...";
 
     outfile.open(filename);
-    outfile << "#W0_VAL=" << W0_VAL << ",PRECESS_TIME=" << PRECESS_TIME
-            << ",PULSE_TIME=" << PULSE_TIME << ",INT_ID=" << INT_ID << "\n";
+    outfile << "#W0_VAL=" << W0_VAL << ",INT_ID=" << INT_ID << "\n";
     outfile.precision(PRECISION);
-    outfile << "#phi,gridMin,polyMin\n";
+    outfile << "#pulseWidth,gridMin,polyMin\n";
 
-    for (int i = 0; i < phaseRange.size(); i++)
+    for (int i = 0; i < tRange.size(); i++)
     {
-        outfile << phaseRange[i] << "," << gridSearchMin[i] << ','
+        outfile << tRange[i] << "," << gridSearchMin[i] << ','
                 << polyFitMin[i] << "\n";
     }
     outfile.close();
